e1a5e028548db340ef989fce590557a882dc0e43
Carlos Ibarra <ceibarrag@gmail.com>
2020-12-11 13:01:42 -0600

    example of for loop

---
+++ for.py
@@ -0,0 +1,10 @@
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+# Lo anterior es equivalente a:
+
+for contador in range(1000):
+    print(contador)
